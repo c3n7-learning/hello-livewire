4c70b2ae2a23125f478fefaa45f4ef502ac1c1dc
Timothy Karani <kartimothy@gmail.com>
2022-03-01 16:16:06 +0300

    debouncing

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -4,7 +4,7 @@
       <h1 class="my-10 text-3xl">Comments</h1>
       <div class="my-4 flex">
         <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placehodler="What's in your mind."
-          wire:model="newComment">
+          wire:model.debounce.500ms="newComment">
         <div class="py-2">
           <button class="p-2 bg-blue-500 w-20 rounded shadow text-white" wire:click="addComment">Add</button>
         </div>
