ed22196edda440b5ab7ca3f4b19a2d3036b70f00
Timothy Karani <kartimothy@gmail.com>
2022-03-02 01:59:29 +0300

    Can navigate

--- resources/views/pagination-links.blade.php
+++ resources/views/pagination-links.blade.php
@@ -7,6 +7,22 @@
         <li class="px-2 py-1 text-center rounded border shadow bg-white cursor-pointer" wire:click="previousPage">Prev
         </li>
       @endif
+
+      @foreach ($elements as $element)
+        <div class="flex">
+          @if (is_array($element))
+            @foreach ($element as $page => $url)
+              @if ($page == $paginator->currentPage())
+                <li class="mx-2 px-2 py-1 text-center rounded border bg-gray-100">{{ $page }}</li>
+              @else
+                <li class="mx-2 px-2 py-1 text-center rounded border bg-white cursor-pointer"
+                  wire:click="gotoPage({{ $page }})">{{ $page }}</li>
+              @endif
+            @endforeach
+          @endif
+        </div>
+      @endforeach
+
       @if ($paginator->hasMorePages())
         <li class="px-2 py-1 text-center rounded border shadow bg-white cursor-pointer" wire:click="nextPage">Next</li>
       @else
