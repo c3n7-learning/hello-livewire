3e7a3387ed573c3b641caa1c7676d8631b5d149a
Timothy Karani <kartimothy@gmail.com>
2022-03-02 12:48:36 +0300

    Showing comments

--- app/Http/Livewire/Tickets.php
+++ app/Http/Livewire/Tickets.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\SupportTicket;
 use Livewire\Component;
 
 class Tickets extends Component
 {
-    public function render()
-    {
-        return view('livewire.tickets');
-    }
+  public function render()
+  {
+    return view('livewire.tickets', [
+      // ::with('creator')->
+      'tickets' => SupportTicket::orderBy('created_at', 'desc')->get()
+    ]);
+  }
 }
--- resources/views/livewire/tickets.blade.php
+++ resources/views/livewire/tickets.blade.php
@@ -1,3 +1,17 @@
 <div class="p-3">
   <h1 class="my-10 text-3xl">Tickets</h1>
+  @foreach ($tickets as $ticket)
+    <div class="rounded border shadow p-3 my-2">
+      <div class="flex justify-between my-2">
+        <div>
+          {{-- <p class="font-bold text-lg">{{ $comment->creator->name }}</p> --}}
+          <p class="mx-3 py-1 text-xs text-gray-500 font-semibold">{{ $ticket->created_at->diffForHumans() }}</p>
+        </div>
+        {{-- <i class="fas fa-times text-red-200 hover:text-red-600 cursor-pointer"
+          wire:click="remove({{ $comment->id }})"></i> --}}
+      </div>
+      <p class="text-gray-800">
+        {{ $ticket->question }} </p>
+    </div>
+  @endforeach
 </div>
