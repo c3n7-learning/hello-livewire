94ef120131688402396c6f00a7b1956887272f8e
Timothy Karani <kartimothy@gmail.com>
2022-03-02 14:40:30 +0300

    Added mutator

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
 
-Route::get('/login', Login::class);
+Route::get('/login', Login::class)->name('login');
 
 Route::get('/register', Register::class);
